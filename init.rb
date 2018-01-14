8fd8030b8a65f103e497bd0bb9c41778a59aca11
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2018-01-14 22:38:35 +0100

    Added safeguards for client secret and id

--- init.rb
+++ init.rb
@@ -1,15 +1,26 @@
 require 'bundler'
 Bundler.require(:default)
 
-Launchy.open("https://www.strava.com/oauth/authorize?client_id=10029&response_type=code&redirect_uri=http://localhost:4567/token_exchange&scope=write&approval_prompt=force");
-
 CLIENT_SECRET="YOUR_STRAVA_CLIENT_SECRET"
+CLIENT_ID="YOUR_CLIENT_ID"
+
+if CLIENT_SECRET == 'YOUR_STRAVA_CLIENT_SECRET'
+	puts "YOU DIDN'T SET YOU CLIENT SECRET"
+	exit
+end
+
+if CLIENT_ID == 'YOUR_CLIENT_ID'
+	puts "YOU DIDN'T SET YOU CLIENT ID"
+	exit
+end
+
+Launchy.open("https://www.strava.com/oauth/authorize?client_id=10029&response_type=code&redirect_uri=http://localhost:4567/token_exchange&scope=write&approval_prompt=force");
 
 get '/token_exchange' do
   code = params[:code]
 
   res = HTTParty.post('https://www.strava.com/oauth/token', body: {
-    client_id: 10029,
+    client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
     code: code    
   })
